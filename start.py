75ed7520d44b53801757d9ecef7a58ae9f3b8e9a
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2023-05-25 08:58:47 +0000

    Wrong syntax is fixed

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from VenvManager import VenvManager
 if __name__ == '__main__':
     venvManager = VenvManager("venv")
 
-    if venvManager.IsEnvironmentCreated is False:
+    if venvManager.IsEnvironmentCreated() is False:
         venvManager.CreateEnvironmentWPackageNames("packages.txt")
     
     venvManager.RunFile("main.py")
