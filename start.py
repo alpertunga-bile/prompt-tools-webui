ede8e8e343abb77a63eff193ef1bc32e90d10d99
alpertunga-bile <kingdom19online@hotmail.com>
2023-05-26 13:36:05 +0300

    Folder check is added to start file

--- start.py
+++ start.py
@@ -1,9 +1,17 @@
 from VenvManager import VenvManager
+from os.path import exists
+from os import mkdir
 
 if __name__ == '__main__':
     venvManager = VenvManager("venv")
 
     if venvManager.IsEnvironmentCreated() is False:
         venvManager.CreateEnvironmentWPackageNames("packages.txt")
+
+    if exists("dataset") is False:
+        mkdir("dataset")
+
+    if exists("prompts") is False:
+        mkdir("prompts")
     
     venvManager.RunFile("main.py")
