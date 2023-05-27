60c9504869c0fa9bf4c6f40712b6bc90ec6a1e50
alpertunga-bile <kingdom19online@hotmail.com>
2023-05-27 18:03:04 +0300

    Generate tab is designed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gradio import Blocks, Column, Tab, Row, Label,  Files, File, Checkbox, Button, Textbox, Slider, Dropdown
+from gradio import Blocks, Column, Tab, Row, Label, Files, File, Checkbox, Button, Textbox, Slider, Dropdown
 from gradio.themes import Monochrome
 from WebUI.ParseUI import Parse, ParseAll
 from WebUI.CivitaiUI import GetMaxPage, Enhance, Show
@@ -15,17 +15,18 @@ with Blocks(title="Prompt Tools WebUI", theme=Monochrome()) as application:
                         )
             with Column():
                 translateCheckbox = Checkbox(value=False, label="Translate", info="Translate the prompts to English")
+                parserProgress = Label(label="Info")
                 parseAllButton = Button(value="Parse All Files In Prompts Folder")
                 parseButton = Button(value="Parse")
-                parserProgress = Label(label="Info")
     with Tab("Civitai"):
         with Row():
             positiveFilenameTextbox = Textbox(label="Positive Filename", value="positive", interactive=True, lines=1)
             negativeFilenameTextbox = Textbox(label="Negative Filename", value="negative", interactive=True, lines=1)
             
         imageLimitSlider = Slider(1, 200, value=1, label="Image Limit Per Page", interactive=True)
-        pageNumberToStart = Slider(1, GetMaxPage() - 1, value=1, label="Page Number To Start", interactive=True)
-        pageNumberToEnd = Slider(2, GetMaxPage(), value=2, label="Page Number To End", interactive=True)
+        with Row():
+            pageNumberToStart = Slider(1, GetMaxPage() - 1, value=1, label="Page Number To Start", interactive=True)
+            pageNumberToEnd = Slider(2, GetMaxPage(), value=2, label="Page Number To End", interactive=True)
         with Row():
             wantedPromptsTextBox = Textbox(label="Wanted Prompts",
                                               value="beautiful, female, breasts, woman, girl, masterpiece",
@@ -48,20 +49,39 @@ with Blocks(title="Prompt Tools WebUI", theme=Monochrome()) as application:
                                     value="All",
                                     label="NSFW",
                                     interactive=True)
+        civitaiProgress = Label(label="Info")
         with Row():
             enhanceButton = Button(value="Enhance")
             civitaiShowButton = Button(value="Show Dataset Folder")
-        civitaiProgress = Label(label="Info")
     with Tab("Train"):
-        modelNameTextbox = Textbox(value="gpt2", label="Model name for text generator model", interactive=True, lines=1)
-        epochSlider = Slider(label="Epochs", minimum=1, maximum=1000, value=10, interactive=True)
-        batchSlider = Slider(label="Batch Size", minimum=1, maximum=512, value=32, interactive=True)
-        modelFolderNameTextbox = Textbox(value="myTextGenerator", label="Model folder name", interactive=True, lines=1)
+        with Row():
+            modelNameTextbox = Textbox(value="gpt2", label="Model name for text generator model", interactive=True, lines=1)
+            modelFolderNameTextbox = Textbox(value="myTextGenerator", label="Model folder name", interactive=True, lines=1)
+        with Row():
+            epochSlider = Slider(label="Epochs", minimum=1, maximum=1000, value=10, interactive=True)
+            batchSlider = Slider(label="Batch Size", minimum=1, maximum=512, value=32, interactive=True)
         datasetFileSelect = File(label="Choose Dataset", file_types=["text"], interactive=True)
-        trainButton = Button(value="Train", interactive=True)
         with Row():
             trainInfoLabel = Label(label="Train Info")
             evaluateInfoLabel = Label(label="Evaluate Info")
+        trainButton = Button(value="Train", interactive=True)
+    with Tab("Generate"):
+        with Row():
+            generateModelFolderNameTextbox = Textbox(label="Model Folder Name", value="myTextGenerator", lines=1, interactive=True)
+            generateModelNameTextbox = Textbox(label="Model Name", value="gpt2", lines=1, interactive=True)
+            generateShowButton = Button(value="Show Dataset Folder")
+        with Row():
+            generateMinLengthSlider = Slider(10, 100, label="Min Length", info="Minimum number of generated tokens", value=10, interactive=True)
+            generateMaxLengthSlider = Slider(50, 300, label="Max Length", info="Maximum number of generated tokens", value=100,interactive=True)
+        with Row():
+            generateDoSampleCheckbox = Checkbox(value=False, label="Do Sample", info='When checked, picks words based on their conditional probability', interactive=True)
+            generateEarlyStopCheckbox = Checkbox(value=False, label="Early Stop", info='When checked, generation finishes if the EOS token is reached', interactive=True)
+        with Row():
+            generateRecursiveLevel = Slider(0, 20, value=0, step=1, label="Recursive Level", interactive=True)
+            generateSelfRecursiveCheckbox = Checkbox(value=False, label="Self Recursive", interactive=True)
+        generateSeedTextbox = Textbox(label="Seed", value="masterpiece", interactive=True)
+        generateGenTextbox = Textbox(label="Generated Text", interactive=False, lines=10)
+        generateGenerateButton = Button("Generate")
 
     # Parse Tab Listeners
     parseButton.click(Parse, inputs=[fileSelect, translateCheckbox], outputs=[parserProgress])
@@ -78,6 +98,9 @@ with Blocks(title="Prompt Tools WebUI", theme=Monochrome()) as application:
     trainButton.click(Train, 
                       [modelNameTextbox, epochSlider, batchSlider, modelFolderNameTextbox, datasetFileSelect],
                       outputs=[trainInfoLabel, evaluateInfoLabel])
+    
+    # Generate Tab Listeners
+    generateShowButton.click(Show)
 
 if __name__ == '__main__':
     application.queue(concurrency_count=4).launch()
